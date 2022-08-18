01a3bd7d86ccb437e07dc616a8fead60450d73f0
MysteriousGrimReaper <hitherehangouts@gmail.com>
2022-08-17 21:17:50 -0700

    spacing i think

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,21 @@
 const EventEmitter = require('events');
 const Discord = require('discord.js');
 require('dotenv').config();
+class discordS extends EventEmitter {
 
-class discordS extends EventEmitter{
-
-    constructor(){
+    constructor() {
         super();
         this.client = new Discord.Client();
-        this.client.on('ready',this.OnDiscordReady);
-        this.client.on('message',this.OnDiscordMessage);
+        this.client.on('ready', this.OnDiscordReady);
+        this.client.on('message', this.OnDiscordMessage);
         this.client.login(process.env.TOKEN);
     }
 
     OnDiscordMessage = (msg) => { //arrow function
-        if(msg.author == this.client.author) return;
+        if (msg.author == this.client.author) return;
     }
 
-    OnDiscordReady =() => { //arrow function
+    OnDiscordReady = () => { //arrow function
         console.log(`Discord client logged in as ${this.client.user.tag}`);
     }
 
