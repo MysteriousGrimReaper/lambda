53e587965484602f53bb25afa87c278fa0437e22
MysteriousGrimReaper <hitherehangouts@gmail.com>
2022-08-25 10:22:04 -0700

    commands
    added some new things to gitignore, added chacha and screen commands

--- commands/twowhosting/screen.js
+++ commands/twowhosting/screen.js
@@ -25,20 +25,27 @@ module.exports = {
                 const dataArray = data.split('\n')[1].split('\t').slice(2);
                 shuffleArray(dataArray)
                 console.log(dataArray);
-                message.channel.send('yay! the responses file exists - generating screen...')
-                const screen_name = String(message.author.username) + getRandomInt(9999)
-                let vote_screen = screen_name
-                dataArray.forEach((item, index) => vote_screen += '\n' + String.fromCharCode(65 + index) + '\t' + item)
-                let vote_screen_message = '```\n' + vote_screen
-                vote_screen_message += '```'
-                fs.writeFile('./votescreens/' + String(message.author.id) + screen_name + '.tsv', vote_screen, err => {
-                    if (err) {
-                        console.error(err)
-                        return
+                message.channel.send('yay! the responses file exists - generating screen... (does not actually generate screen rn. :troll:)')
+                let num_factors = [] // number of factors
+                for (let i = 1; i <= dataArray.length; i++) {
+                    if (dataArray.length % i == 0) {
+                        num_factors.push(i)
                     }
-                    // file written successfully
-                })
-                message.channel.send(vote_screen_message)
+                }
+                console.log(num_factors)
+                    /* const screen_name = String(message.author.username) + getRandomInt(9999)
+                    let vote_screen = screen_name
+                    dataArray.forEach((item, index) => vote_screen += '\n' + String.fromCharCode(65 + index) + '\t' + item)
+                    let vote_screen_message = '```\n' + vote_screen
+                    vote_screen_message += '```'
+                    fs.writeFile('./votescreens/' + String(message.author.id) + screen_name + '.tsv', vote_screen, err => {
+                        if (err) {
+                            console.error(err)
+                            return
+                        }
+                        // file written successfully
+                    })
+                    message.channel.send(vote_screen_message)*/
             });
         } else {
             message.channel.send('does not exist :pensive:')
